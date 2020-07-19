0d18e41b0f5455294337a47cb0eef14f6628c451
Oleg <oleg.artemov@whykingz.com>
2020-07-19 12:16:13 +0300

    add auto comments for wifes posts

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
+import random
 from time import sleep
 
 from instabot import Bot
 
-from settings import USERNAME, PASSWORD, WIFE_ACCOUNT
+from settings import USERNAME, PASSWORD, WIFE_ACCOUNT, COMMENTS_FILE
+
+LAST_POSTS_COUNT = 10
 
 
-def like_my_wife(username, password, user_id):
+def like_and_comment_my_wife(username: str, password: str, user_id: str, comment: str, last_commented: list) -> list:
     bot = Bot()
     bot.login(username=username, password=password)
+
     bot.like_user(user_id, filtration=False)
+
+    medias = bot.get_last_user_medias(WIFE_ACCOUNT, LAST_POSTS_COUNT)
+    not_commented = set(medias) - set(last_commented)
+    for media_id in not_commented:
+        if not bot.is_commented(media_id):
+            bot.comment(media_id, comment)
+        last_commented.append(media_id)
+
+    return last_commented[-LAST_POSTS_COUNT:]
 
 
 if __name__ == '__main__':
+    last_commented_medias = []
+
+    with open(COMMENTS_FILE, 'r') as f:
+        lines = [x.strip('\n') for x in f.readlines()]
+        comments = [x for x in lines if x]
+
     while True:
-        like_my_wife(username=USERNAME, password=PASSWORD, user_id=WIFE_ACCOUNT)
+        last_commented_medias = like_and_comment_my_wife(
+            username=USERNAME,
+            password=PASSWORD,
+            user_id=WIFE_ACCOUNT,
+            comment=random.choice(comments),
+            last_commented=last_commented_medias
+        )
+
         sleep(1800)
--- settings.py
+++ settings.py
@@ -3,3 +3,4 @@ import os
 USERNAME = os.environ['USERNAME']
 PASSWORD = os.environ['PASSWORD']
 WIFE_ACCOUNT = os.environ['WIFE_ACCOUNT']
+COMMENTS_FILE = 'my_comments.txt'
