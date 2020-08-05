84af6a66b79f1e81624ba05fe6cf4acedb2a1b8f
Oleg <oleg.artemov@whykingz.com>
2020-08-05 23:40:10 +0300

    add flag for enable/disable comments mode

--- main.py
+++ main.py
@@ -10,11 +10,15 @@ LAST_POSTS_COUNT = 10
 SLEEP_TIMEOUT = 30 * 60  # half hour
 
 
-def like_and_comment_my_wife(username: str, password: str, user_id: str, comments: list, last_commented: list) -> list:
+def like_and_comment_my_wife(username: str, password: str, user_id: str, comments: list, last_commented: list,
+                             comments_enabled=False) -> list:
     bot = Bot()
     bot.login(username=username, password=password)
 
     bot.like_user(user_id, filtration=False)
+
+    if not comments_enabled:
+        return last_commented
 
     medias = bot.get_last_user_medias(WIFE_ACCOUNT, LAST_POSTS_COUNT)
     not_commented = set(medias) - set(last_commented)
